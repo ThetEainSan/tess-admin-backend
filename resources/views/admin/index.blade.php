07681dff109e9c2acc932b061a12c8bbb68962d9
aungnaingphyoe <aungnaingphyo@xsphere.co>
2022-07-15 15:50:36 +0630

    admin index page update

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.master')
 @section('title','Admins')
-
+@section('styles')
+    <link href="{{ asset('assets/vendors/datatables/dataTables.bootstrap.min.css') }}" rel="stylesheet">
+@endsection
 @section('content')
 
 @endsection
